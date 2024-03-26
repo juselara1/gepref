02114ba3427b3acc35289b948158b1f329884313
Juan Lara <julara@unal.edu.co>
2024-03-25 19:21:48 -0500

    feat: added list preprocessors for test

---
+++ src/gepref/list.py
@@ -0,0 +1,14 @@
+from typing import List
+from gepref.preprocessor import GenericPreprocessor
+from gepref.step import AbstractStep
+
+T = List[float]
+class ListPreprocessor(GenericPreprocessor[T]): ...
+
+class IdentityStep(AbstractStep[T]):
+    def call(self, data: T) -> T:
+        return data
+
+class SquareStep(AbstractStep[List]):
+    def call(self, data: T) -> T:
+        return data
